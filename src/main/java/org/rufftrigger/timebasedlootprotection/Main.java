8023beae0cd4e6e0dc5b934c43ceaf47953c90c0
RuffTrigger <jeanmduus@gmail.com>
2024-07-05 15:07:23 +0200

    v1.0

--- src/main/java/org/rufftrigger/timebasedlootprotection/Main.java
+++ src/main/java/org/rufftrigger/timebasedlootprotection/Main.java
@@ -3,7 +3,6 @@ package org.rufftrigger.timebasedlootprotection;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
@@ -12,14 +11,13 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockIgniteEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.entity.EntitySpawnEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.event.block.Action;
 
 import java.util.Collection;
 import java.util.UUID;
@@ -40,11 +38,15 @@ public class Main extends JavaPlugin implements Listener {
         for (ItemData itemData : protectedItems) {
             if (itemData.getExpirationTime() > System.currentTimeMillis()) {
                 Location location = new Location(Bukkit.getWorld(itemData.getWorldUUID()), itemData.getDropLocationX(), itemData.getDropLocationY(), itemData.getDropLocationZ());
-                ItemStack itemStack = new ItemStack(Material.DIRT); // Placeholder, you'll need to store and retrieve actual item data
-                Item item = location.getWorld().dropItem(location, itemStack);
-                item.setInvulnerable(true); // Set item as invulnerable initially
-                long delay = ((itemData.getExpirationTime() - System.currentTimeMillis()) / 50); // Convert milliseconds to ticks
-                scheduleProtectionRemoval(item.getUniqueId(), delay);
+                ItemStack itemStack = new ItemStack(Material.DIRT); // Placeholder, replace with actual item data
+                if (location.getWorld() != null) {
+                    Item item = location.getWorld().dropItem(location, itemStack);
+                    if (item != null) {
+                        item.setInvulnerable(true); // Set item as invulnerable initially
+                        long delay = ((itemData.getExpirationTime() - System.currentTimeMillis()) / 50); // Convert milliseconds to ticks
+                        scheduleProtectionRemoval(item.getUniqueId(), delay);
+                    }
+                }
             } else {
                 databaseHandler.removeItemProtection(itemData.getItemUUID());
             }
@@ -57,11 +59,8 @@ public class Main extends JavaPlugin implements Listener {
     }
 
     private void loadConfig() {
-        FileConfiguration config = getConfig();
-        config.addDefault("protection_time_minutes", 5);
-        config.options().copyDefaults(true);
-        saveConfig();
-        protectionTimeMinutes = config.getInt("protection_time_minutes");
+        saveDefaultConfig();
+        this.protectionTimeMinutes = getConfig().getInt("protection_time_minutes", 5);
     }
 
     @EventHandler
@@ -69,13 +68,13 @@ public class Main extends JavaPlugin implements Listener {
         if (event.getEntity() instanceof Item) {
             Item item = (Item) event.getEntity();
             UUID itemUUID = item.getUniqueId();
-            long expirationTime = System.currentTimeMillis() + (protectionTimeMinutes * 60 * 1000); // Convert minutes to milliseconds
+            long expirationTime = System.currentTimeMillis() + (protectionTimeMinutes * 60L * 1000L); // Convert minutes to milliseconds
             double dropLocationX = item.getLocation().getX();
             double dropLocationY = item.getLocation().getY();
             double dropLocationZ = item.getLocation().getZ();
             UUID worldUUID = item.getWorld().getUID();
             databaseHandler.addItemProtection(itemUUID, null, expirationTime, dropLocationX, dropLocationY, dropLocationZ, worldUUID);
-            scheduleProtectionRemoval(itemUUID, protectionTimeMinutes * 60 * 20); // Convert minutes to ticks
+            scheduleProtectionRemoval(itemUUID, protectionTimeMinutes * 60L * 20L); // Convert minutes to ticks
         }
     }
 
@@ -85,13 +84,13 @@ public class Main extends JavaPlugin implements Listener {
         Player player = event.getPlayer();
         UUID itemUUID = item.getUniqueId();
         UUID ownerUUID = player.getUniqueId();
-        long expirationTime = System.currentTimeMillis() + (protectionTimeMinutes * 60 * 1000); // Convert minutes to milliseconds
+        long expirationTime = System.currentTimeMillis() + (protectionTimeMinutes * 60L * 1000L); // Convert minutes to milliseconds
         double dropLocationX = item.getLocation().getX();
         double dropLocationY = item.getLocation().getY();
         double dropLocationZ = item.getLocation().getZ();
         UUID worldUUID = item.getWorld().getUID();
         databaseHandler.addItemProtection(itemUUID, ownerUUID, expirationTime, dropLocationX, dropLocationY, dropLocationZ, worldUUID);
-        scheduleProtectionRemoval(itemUUID, protectionTimeMinutes * 60 * 20); // Convert minutes to ticks
+        scheduleProtectionRemoval(itemUUID, protectionTimeMinutes * 60L * 20L); // Convert minutes to ticks
     }
 
     @EventHandler
@@ -128,15 +127,18 @@ public class Main extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onEntityExplode(EntityExplodeEvent event) {
-        Collection<Entity> nearbyEntities = event.getLocation().getWorld().getNearbyEntities(event.getLocation(), 10, 10, 10);
-        for (Entity entity : nearbyEntities) {
-            if (entity instanceof Item) {
-                Item item = (Item) entity;
-                UUID itemUUID = item.getUniqueId();
-                Long expirationTime = databaseHandler.getExpirationTime(itemUUID);
-                if (expirationTime != null && expirationTime > System.currentTimeMillis()) {
-                    event.setCancelled(true);
-                    return;
+        Location location = event.getLocation();
+        if (location.getWorld() != null) {
+            Collection<Entity> nearbyEntities = location.getWorld().getNearbyEntities(location, 10, 10, 10);
+            for (Entity entity : nearbyEntities) {
+                if (entity instanceof Item) {
+                    Item item = (Item) entity;
+                    UUID itemUUID = item.getUniqueId();
+                    Long expirationTime = databaseHandler.getExpirationTime(itemUUID);
+                    if (expirationTime != null && expirationTime > System.currentTimeMillis()) {
+                        event.setCancelled(true);
+                        return;
+                    }
                 }
             }
         }
@@ -145,15 +147,17 @@ public class Main extends JavaPlugin implements Listener {
     @EventHandler
     public void onBlockIgnite(BlockIgniteEvent event) {
         Location location = event.getBlock().getLocation();
-        Collection<Entity> nearbyEntities = location.getWorld().getNearbyEntities(location, 10, 10, 10);
-        for (Entity entity : nearbyEntities) {
-            if (entity instanceof Item) {
-                Item item = (Item) entity;
-                UUID itemUUID = item.getUniqueId();
-                Long expirationTime = databaseHandler.getExpirationTime(itemUUID);
-                if (expirationTime != null && expirationTime > System.currentTimeMillis()) {
-                    event.setCancelled(true);
-                    return;
+        if (location.getWorld() != null) {
+            Collection<Entity> nearbyEntities = location.getWorld().getNearbyEntities(location, 10, 10, 10);
+            for (Entity entity : nearbyEntities) {
+                if (entity instanceof Item) {
+                    Item item = (Item) entity;
+                    UUID itemUUID = item.getUniqueId();
+                    Long expirationTime = databaseHandler.getExpirationTime(itemUUID);
+                    if (expirationTime != null && expirationTime > System.currentTimeMillis()) {
+                        event.setCancelled(true);
+                        return;
+                    }
                 }
             }
         }
@@ -161,20 +165,21 @@ public class Main extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
-        Player player = event.getPlayer();
-        if (event.getClickedBlock() != null && (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.LEFT_CLICK_BLOCK))) {
+        if (event.getClickedBlock() != null && (event.getAction().equals(org.bukkit.event.block.Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(org.bukkit.event.block.Action.LEFT_CLICK_BLOCK))) {
             Material material = event.getClickedBlock().getType();
             if (material == Material.TNT || material == Material.FIRE || material == Material.LAVA) {
                 Location location = event.getClickedBlock().getLocation();
-                Collection<Entity> nearbyEntities = location.getWorld().getNearbyEntities(location, 10, 10, 10);
-                for (Entity entity : nearbyEntities) {
-                    if (entity instanceof Item) {
-                        Item item = (Item) entity;
-                        UUID itemUUID = item.getUniqueId();
-                        Long expirationTime = databaseHandler.getExpirationTime(itemUUID);
-                        if (expirationTime != null && expirationTime > System.currentTimeMillis()) {
-                            event.setCancelled(true);
-                            return;
+                if (location.getWorld() != null) {
+                    Collection<Entity> nearbyEntities = location.getWorld().getNearbyEntities(location, 10, 10, 10);
+                    for (Entity entity : nearbyEntities) {
+                        if (entity instanceof Item) {
+                            Item item = (Item) entity;
+                            UUID itemUUID = item.getUniqueId();
+                            Long expirationTime = databaseHandler.getExpirationTime(itemUUID);
+                            if (expirationTime != null && expirationTime > System.currentTimeMillis()) {
+                                event.setCancelled(true);
+                                return;
+                            }
                         }
                     }
                 }
